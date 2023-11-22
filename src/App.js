091c2b86085ffd370201dd4b50ae80da4a03770a
Sammy Tran <sammy.tran1@ucalgary.ca>
2023-11-22 11:53:36 -0700

    Create user in database

--- src/App.js
+++ src/App.js
@@ -3,20 +3,42 @@ import { useEffect, useState } from 'react';
 import { onAuthStateChanged} from "firebase/auth";
 import MainDisplay from "./MainDisplay"
 import AuthPage from './AuthPage';
-import {auth} from "./backend/firebase";
+import {auth, firestore} from "./backend/firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 function App() {
   const [currentUser, setCurrentUser] = useState({});
 
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) =>{
+    const unsub = onAuthStateChanged(auth, async (user) =>{
       setCurrentUser(user);
+      if (user) {
+        await createUserDocument(user);
+      }
     });
 
     return () => {
       unsub();
     }
   }, [])
+
+  const createUserDocument = async (user) => {
+    const userRef = doc(firestore, "users", user.uid);
+    
+    try{
+      const doc = await getDoc(userRef);
+
+      if (!doc.exists()){
+        await setDoc(userRef, {
+          username: user.displayName,
+          email: user.email
+        })
+        console.log("Created");
+      }
+    }catch(error) {
+      console.error("Couldn't create document", error);
+    }
+  }
 
   return (
     <section>
